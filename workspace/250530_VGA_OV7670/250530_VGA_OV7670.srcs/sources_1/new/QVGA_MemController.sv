095eb343d214ee10491a6614430de5e58fda3ffa
minseo0511 <minseo99827@gmail.com>
2025-06-02 15:53:27 +0900

    Auto commit for modified projects on 2025-06-02 15:53

--- workspace/250530_VGA_OV7670/250530_VGA_OV7670.srcs/sources_1/new/QVGA_MemController.sv
+++ workspace/250530_VGA_OV7670/250530_VGA_OV7670.srcs/sources_1/new/QVGA_MemController.sv
@@ -18,10 +18,11 @@ module QVGA_MemController (
     output logic [ 3:0] blue_port
 );
     logic display_en;
+    logic [16:0] raddr_rom, raddr_ram;
     logic [15:0] data_rom;
 
     rom U_bg_rom(
-        .addr(rAddr),
+        .addr(raddr_rom),
         .data(data_rom)
     );
 
@@ -31,24 +32,24 @@ module QVGA_MemController (
     assign d_en = display_en;
 
     //assign rAddr = display_en ? (y_pixel * 320 + x_pixel) : 0;
-
+    assign raddr_rom = display_en ? (y_pixel * 320 + x_pixel) : 0;
+    assign rAddr = sw[1] ? raddr_rom : raddr_ram;
 
      always_comb begin
         if(sw[0]) begin
             display_en = (x_pixel < 640 && y_pixel < 480);
-            rAddr = display_en ? (320 * (y_pixel/2) + (x_pixel/2)) : 0;
+            raddr_ram = display_en ? (320 * (y_pixel/2) + (x_pixel/2)) : 0;
             {red_port, green_port, blue_port} = {rData[15:12], rData[10:7], rData[4:1]};
         end
         else if(sw[1]) begin
             if((rData[15:12] < 4'b0110) && (rData[4:1] < 4'b0110) && (rData[10:7] >= 4'b1100)) begin
                 display_en = (x_pixel < 320 && y_pixel < 240);
-                rAddr = display_en ? (y_pixel * 320 + x_pixel) : 0;
                 {red_port, green_port, blue_port} = {data_rom[15:12], data_rom[10:7], data_rom[4:1]};
             end
         end
         else begin
             display_en = (x_pixel < 320 && y_pixel < 240);
-            rAddr = display_en ? (y_pixel * 320 + x_pixel) : 0;
+            raddr_ram = display_en ? (y_pixel * 320 + x_pixel) : 0;
             {red_port, green_port, blue_port} = {rData[15:12], rData[10:7], rData[4:1]};
         end
     end
